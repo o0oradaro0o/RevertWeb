f171daf8a018422fc937b199398d22babb77a8cf
Phips Peter <pspeter333@gmail.com>
2021-04-18 14:00:56 -0700

    Add feather
    Use feather for icons.

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
+const feather = require("feather-icons");
+
 module.exports = (config) => {
   config.addPassthroughCopy("Build");
   config.addPassthroughCopy("TemplateData");
+  config.addShortcode("icon", (icon) => feather.icons[icon].toSvg());
 };
