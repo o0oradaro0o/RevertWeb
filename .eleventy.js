43f0709cb4b93b015205cc9217499f98c3e5ab0a
Phips Peter <pspeter333@gmail.com>
2021-04-18 18:03:08 -0700

    Create updates page
    Make sure the page has the correct configuration.

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,13 @@ const feather = require("feather-icons");
 module.exports = (config) => {
   config.addPassthroughCopy("Build");
   config.addPassthroughCopy("TemplateData");
-  config.addPassthroughCopy("tailwind.css");
-  config.addShortcode("icon", (icon) => feather.icons[icon].toSvg());
-  config.setUseGitIgnore(false);
+  config.addShortcode("date", (date) => new Date(date).toLocaleDateString());
+  config.addShortcode("icon", (name, size) => {
+    const icon = feather.icons[name];
+    if (!icon) {
+      throw new Error(`Could not find icon ${name}.`);
+    }
+    const options = size ? { width: size, height: size } : undefined;
+    return icon.toSvg(options);
+  });
 };
